0eab4fdf7b5d8e37a1d6392759b59662aed5d9b9
Andreas Prodromou <prodromou.andreas@gmail.com>
2017-01-25 09:57:46 -0800

    Modified test-example

--- Tests/test-example/run.sh
+++ Tests/test-example/run.sh
@@ -9,13 +9,14 @@ LIB_DIR=/lib231
 # path to the test directory
 TEST_DIR=.
 
+$LLVM_BIN/clang -c -O0 $TEST_DIR/test1.c -emit-llvm -S -o /tmp/test1-c.ll
 $LLVM_BIN/clang++ -c -O0 $TEST_DIR/test1.cpp -emit-llvm -S -o /tmp/test1.ll
 $LLVM_BIN/clang++ -c $LIB_DIR/lib231.cpp -emit-llvm -S -o /tmp/lib231.ll
 $LLVM_BIN/clang++ -c $TEST_DIR/test1-main.cpp -emit-llvm -S -o /tmp/test1-main.ll
 
-$LLVM_BIN/opt -load $LIB_DIR/CSE231.so -cse231-csi < /tmp/test1.ll > /dev/null 2> /tmp/csi.result
-$LLVM_BIN/opt -load $LIB_DIR/CSE231.so -cse231-cdi < /tmp/test1.ll -o /tmp/test1-cdi.bc
-$LLVM_BIN/opt -load $LIB_DIR/CSE231.so -cse231-bb < /tmp/test1.ll -o /tmp/test1-bb.bc
+$LLVM_BIN/opt -load $LLVM_SO/CSE231.so -cse231-csi < /tmp/test1-c.ll > /dev/null 2> /tmp/csi.result
+$LLVM_BIN/opt -load $LLVM_SO/CSE231.so -cse231-cdi < /tmp/test1.ll -o /tmp/test1-cdi.bc
+$LLVM_BIN/opt -load $LLVM_SO/CSE231.so -cse231-bb < /tmp/test1.ll -o /tmp/test1-bb.bc
 
 $LLVM_BIN/llvm-dis /tmp/test1-cdi.bc
 $LLVM_BIN/llvm-dis /tmp/test1-bb.bc
