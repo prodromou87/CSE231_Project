acb1c811f50dda90c22d9f2e1d1cce7521fa41f8
Andreas Prodromou <prodromou.andreas@gmail.com>
2019-01-10 11:27:47 +0200

    Updated

--- mount_and_launch.sh
+++ mount_and_launch.sh
@@ -1,14 +1,13 @@
 #!/bin/bash
 
 SOURCE_PATH=`pwd`/Passes
-TESTS_PATH=`pwd`/Tests
 OUTPUT_PATH=`pwd`/Output
 
 # MOUNT POINTS IN CONTAINER: 
 # --------------------------
 #
 # SOURCE CODE: /LLVM_ROOT/llvm/lib/Transforms/CSE231_Project
-# TESTS: /tests
+# TESTS: /tests --> Not mounted anymore
 # OUTPUT: /output
 
-docker run --rm -it -v ${SOURCE_PATH}:/LLVM_ROOT/llvm/lib/Transforms/CSE231_Project -v ${TESTS_PATH}:/tests -v ${OUTPUT_PATH}:/output prodromou87/llvm:5.0.1
+docker run --rm -it -v ${SOURCE_PATH}:/LLVM_ROOT/llvm/lib/Transforms/CSE231_Project -v ${OUTPUT_PATH}:/output prodromou87/llvm:8 /bin/bash
